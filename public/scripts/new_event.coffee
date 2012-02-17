a7fb9a4166b0005de0870514ac8bc37ffa0cb37a
Ladislav Solc <ladislav.solc@gmail.com>
2012-02-17 08:13:50 +0100

    20 - Create a template for new events and display it

--- public/scripts/new_event.coffee
+++ public/scripts/new_event.coffee
@@ -1,16 +1,46 @@
-define [], () -> 
+define [
+	'text!templates/events/new_event.tmpl'
+	], (new_event_text) -> 
 	NewEvent = Backbone.Model.extend
-		title : 'title'	
-		description: ''
-		initialize: () -> {}	
+		initialize: () ->
+
+		
+	NewEventView = Backbone.View.extend
+		el: '#new .current',
+		initialize: () ->		
+			this.template = Handlebars.compile(new_event_text)
+			
+		render: () ->			
+			html_string = this.template({display_name: new_event.get('display_name')})
+			$(this.el).html(html_string)
+			$("select").chosen()
+			#$("select").chosen().change(() -> alert('fired by chosen'))	
+			
+
+
+	new_event = new NewEvent({display_name: "Nuria"})
+	
+	new_event_view = new NewEventView()	
+	new_event_view.render()
 	
 	
-	new_event = new NewEvent({title: "Zajit do servisu", description: "Some text........" })
-	$("select").chosen()
-	$("select").chosen().change(() -> alert('fired by chosen'))
 	
 	
 	
+	
+	
+	
+	###
+	members = $.getJSON "/members", (result) ->
+			options = $("#options")
+			$.each result, () -> 
+				options.append($("<option />")).val(this.id).text(this.display_name)###
+	
+			
+	
+	
+	
+
 	
 	
 	
--- public/scripts/new_event.js
+++ public/scripts/new_event.js
@@ -1,22 +1,35 @@
 (function() {
 
-  define([], function() {
-    var NewEvent, new_event;
+  define(['text!templates/events/new_event.tmpl'], function(new_event_text) {
+    var NewEvent, NewEventView, new_event, new_event_view;
     NewEvent = Backbone.Model.extend({
-      title: 'title',
-      description: '',
+      initialize: function() {}
+    });
+    NewEventView = Backbone.View.extend({
+      el: '#new .current',
       initialize: function() {
-        return {};
+        return this.template = Handlebars.compile(new_event_text);
+      },
+      render: function() {
+        var html_string;
+        html_string = this.template({
+          display_name: new_event.get('display_name')
+        });
+        $(this.el).html(html_string);
+        return $("select").chosen();
       }
     });
     new_event = new NewEvent({
-      title: "Zajit do servisu",
-      description: "Some text........"
+      display_name: "Nuria"
     });
-    $("select").chosen();
-    return $("select").chosen().change(function() {
-      return alert('fired by chosen');
-    });
+    new_event_view = new NewEventView();
+    return new_event_view.render();
+    /*
+    	members = $.getJSON "/members", (result) ->
+    			options = $("#options")
+    			$.each result, () -> 
+    				options.append($("<option />")).val(this.id).text(this.display_name)
+    */
   });
 
 }).call(this);
