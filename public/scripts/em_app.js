2dba1e1e686173ffcca99cd8e928759b7cf5ddf2
Ladislav Solc <ladislav.solc@gmail.com>
2012-06-03 17:47:20 +0200

    Minor changes

--- public/scripts/em_app.js
+++ public/scripts/em_app.js
@@ -1,78 +1,108 @@
 define(
-  [
-  ],
+	[
+	],
 
-  function() {
-  
-    window.App = Em.Application.create();
+	function() {
+		window.App = Em.Application.create();
 
-    App.CalEvent  = Ember.Resource.extend({
-      resourceUrl:        '/events',
-      resourceName:       'event',
-      resourceProperties: ['title', 'description']
-    });
-    App.Member  = Ember.Resource.extend({
-      resourceUrl:        '/members',
-      resourceName:       'member'
-    });
+		App.CalEvent  = Ember.Resource.extend({
+			resourceUrl:        '/events',
+			resourceName:       'event',
+			resourceProperties: ['title', 'description']
+		});
+		App.Member  = Ember.Resource.extend({
+			resourceUrl:        '/members',
+			resourceName:       'member'
+		});
 
-    App.membersController = Ember.ResourceController.create({
-      resourceType: App.Member
-    });
-
-
-    App.calEventsController = Ember.ResourceController.create({
-      resourceType: App.CalEvent
-    });
-
-    App.MainView = Em.View.extend({
-      templateName: 'main'
-    });
-
-    App.MembersView = Em.CollectionView.extend({
-      contentBinding: 'App.membersController.content',
-      itemViewClass: Em.View.extend({
-        templateName: 'member-listItem'
-      })
-    });
+		App.membersController = Ember.ResourceController.create({
+			resourceType: App.Member
+		});
 
 
+		App.calEventsController = Ember.ResourceController.create({
+			resourceType: App.CalEvent
+		});
 
-    App.CalEventsView = Em.CollectionView.extend({
-      contentBinding: 'App.calEventsController.content',
-      itemViewClass: Em.View.extend({
-        templateName: 'calEvent-listItem'
-      })
-    });
+		App.MainView = Em.View.extend({
+			templateName: 'main',
+			newMember: function(event) {
+				//App.MembersFormView.create().appendTo('.detail_form');
+				App.detailFormContainer.get('childViews').pushObject(App.MembersFormView.create())
+				alert('Show member form');
+			}
+		});		
+
+		App.MembersView = Em.CollectionView.extend({
+			contentBinding: 'App.membersController.content',
+			itemViewClass: Em.View.extend({
+				templateName: 'member-listItem'
+			})
+		});
+		
+		App.MembersFormView =  Em.View.extend({
+			templateName: 'member-form',
+			////
+            click: function() {
+                 alert("ClickableView was clicked!");
+            }
+			////
+			
+		});
+		
+		
+		
+		/*
+		App.AnotherViewClass = Ember.View.extend({
+				    template: Ember.Handlebars.compile("Another view")
+				  });*/
+		
+		
+		
+		App.detailFormContainer = Em.ContainerView.create({
+			childViews: [],
+			member_form: App.MembersFormView.create()
+		});	
+		//App.detailFormContainer.get('childViews').removeObject(App.detailFormContainer.get('member_form'))	
+		//App.detailFormContainer.get('childViews').pushObject(App.MembersFormView.create())
+		
+				
+		
+		
+
+		App.CalEventsView = Em.CollectionView.extend({
+			contentBinding: 'App.calEventsController.content',
+			itemViewClass: Em.View.extend({
+				templateName: 'calEvent-listItem'
+			})
+		});
 
 
 
-    $.when(App.calEventsController.findAll(), App.membersController.findAll()).then(function() {
-      console.log(App.calEventsController.content);
-      console.log(App.membersController.content);
-      App.MainView.create().append();
-    });
-            
-
-
-
-            
+		$.when(App.calEventsController.findAll(), App.membersController.findAll()).then(function() {
+			console.log(App.calEventsController.content);
+			console.log(App.membersController.content);
+			App.MainView.create().append();
+			
+			App.detailFormContainer.appendTo('.detail_form');
+			//App.MembersFormView.create().appendTo('.detail_form');
+		});
     
-    App.HomeState = Em.State.extend({
-      route: "/",
-      setupContext: function(manager) {
-        manager.set('currentView', App.CalEventsView.create({
-          controller: manager.get('calEventsController')
-        }));
-      }
-    });
+		App.HomeState = Em.State.extend({
+			route: "/",
+			setupContext: function(manager) {
+				manager.set('currentView', App.CalEventsView.create({
+					controller: manager.get('calEventsController')
+				}));
+			}
+		});
 
 
-    App.stateManager = Ember.StateManager.create({
-      start: App.HomeState
-    });
+		App.stateManager = Ember.StateManager.create({
+			start: App.HomeState
+		});
     
-    App.stateManager.getPath('currentState.name');
+		App.stateManager.getPath('currentState.name');
 
-  }
+	}
 );
